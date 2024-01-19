2ba14c00b9213a9ce29b548e1df7561bd523c5e7
Shaik Ahmad Nawaz <nawazcode@gmail.com>
2024-01-19 20:34:38 +0530

    Implemented user authentication with roles (admin, advertiser, content creator)

--- server/src/app.js
+++ server/src/app.js
@@ -1,6 +1,7 @@
 import express from "express";
 import cors from "cors";
 import cookieParser from "cookie-parser";
+import userRouter from "../src/routes/user.routes.js";
 
 const app = express();
 
@@ -14,5 +15,7 @@ app.use(
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
+
+app.use("/api/v1/users", userRouter);
 
 export default app;
---
+++ server/src/middlewares/auth.middleware.js
@@ -0,0 +1,41 @@
+import { ApiError } from "../utils/ApiError.js";
+import { asyncHandler } from "../utils/asyncHandler.js";
+import jwt from "jsonwebtoken";
+import User from "../models/user.model.js";
+
+const verifyJWT = asyncHandler(async (req, _, next) => {
+  try {
+    const token =
+      req.cookies?.accessToken ||
+      req.header("Authorization")?.replace("Bearer ", "");
+
+    if (!token) {
+      throw new ApiError(401, "Unauthorized");
+    }
+
+    const decodedToken = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET);
+
+    const user = await User.findById(decodedToken?._id).select(
+      "-password refreshToken"
+    );
+
+    if (!user) {
+      throw new ApiError(401, "Unauthorized");
+    }
+
+    req.user = user;
+    next();
+  } catch (error) {
+    next(error);
+  }
+});
+
+const checkRole = (role) => (req, _, next) => {
+  const userRole = req.user.role;
+  if (userRole !== role) {
+    throw new ApiError(403, "Forbidden");
+  }
+  next();
+};
+
+export { verifyJWT, checkRole };
